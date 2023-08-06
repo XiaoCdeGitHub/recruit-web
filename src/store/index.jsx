8e57d2a461933c5dcd659c2a7a6c5c98e690fb28
DoubleLiHao <=2998000782@qq.com>
2023-08-06 15:36:53 +0800

    repair

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,7 +2,7 @@
  * @Author: DoubleLiHao =2998000782@qq.com
  * @Date: 2023-08-03 17:03:55
  * @LastEditors: DoubleLiHao =2998000782@qq.com
- * @LastEditTime: 2023-08-04 21:17:23
+ * @LastEditTime: 2023-08-06 15:35:27
  * @FilePath: \yzyy-web\src\store\index.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,13 +12,11 @@ import { numberApiSlice } from "./numberApi";
 // 面试主页的数据
 import { interviewSlice } from "./interviewSlice";
 
-import { numberApiSlice } from "./numberApi";
 import { loginApi } from "./loginApi";
 import { loginInfoSlice } from "./loginInfoSlice";
 
 // 面试排班的数据
 import { timeTableSlice } from "./TimeTableSlice";
-
 
 export const store = configureStore({
   // 把自定义的Slice放到这个对象里

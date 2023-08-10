aa6a372c70553eded7ebe5d70ff654dea693b42e
DoubleLiHao <=2998000782@qq.com>
2023-08-10 12:41:27 +0800

    page-interview-repair

--- src/pages/Interview/Component/ItvStuForm/StuInfoDetail.jsx
+++ src/pages/Interview/Component/ItvStuForm/StuInfoDetail.jsx
@@ -2,7 +2,7 @@
  * @Author: DoubleLiHao =2998000782@qq.com
  * @Date: 2023-08-05 20:49:25
  * @LastEditors: DoubleLiHao =2998000782@qq.com
- * @LastEditTime: 2023-08-09 20:55:37
+ * @LastEditTime: 2023-08-10 12:32:43
  * @FilePath: \yzyy-web\src\pages\Interview\Component\ItvStuForm\StuInfoDetail.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,7 @@ import { useNavigate } from "react-router-dom";
 import styles from "./StuInfoDetail.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { Backdrop } from "../../../../UI/Backdrop/Backdrop";
+import { useRef } from "react";
 import {
   showImgBackdrop,
   hiddenImgBackdrop,
@@ -17,24 +18,54 @@ import {
 
 export const StuInfoDetail = () => {
   const { isEnterImgBackdrop } = useSelector((state) => state.interviewSlice);
-  console.log(isEnterImgBackdrop);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const clickHandler = () => {
     navigate("/interview");
   };
-  const showImg = () => {
+  const showImg = (e) => {
+    e.preventDefault();
     dispatch(showImgBackdrop());
   };
   const hiddenImg = () => {
     dispatch(hiddenImgBackdrop());
+  };
+
+  // 获取轮播图box的节点
+  const imgbox = useRef();
+  // 移动轮播图box
+  const moveImgbox = (e) => {
+    // 获取imgbox节点left的值
+    const numberLeft = (left) => {
+      if (left == "") {
+        console.log("值为空");
+        return 0;
+      } else {
+        return +left.replace("vw", "");
+      }
+    };
+    // 判断左滑还是右滑
+    const type = e.target.getAttribute("name");
+
+    let dom = imgbox.current;
+    const left = imgbox.current.style.left;
+    if (type === "left") {
+      let number = numberLeft(left) + 50;
+      dom.style.left = number + "vw";
+    } else {
+      let number = numberLeft(left) - 50;
+      dom.style.left = number + "vw";
+    }
   };
   return (
     <>
       <div className={styles.box}>
         <div className={styles.top}>
           <div className={styles.img} onClick={showImg}>
-            <img />
+            <img
+              className={styles.picture}
+              src="https://img-doubleli.oss-cn-hangzhou.aliyuncs.com/8eba916a32dd29152e3667bf5ccf2d6.png"
+            />
           </div>
           <div className={styles.baseInfo}>
             <div>
@@ -60,11 +91,23 @@ export const StuInfoDetail = () => {
       </div>
       {isEnterImgBackdrop && (
         <Backdrop>
-          <div onClick={hiddenImg}>
-            <p className={styles.imgBack}>返回</p>
+          <div onClick={hiddenImg} className={styles.imgBack}>
+            <p>返回</p>
           </div>
-
-          <img className={styles.backdropImg} />
+          <div className={styles.imgBox}>
+            <div className={styles.imgbox} ref={imgbox}>
+              <img src="https://img-doubleli.oss-cn-hangzhou.aliyuncs.com/7bf1d4e58670805d20273ce31cea2b6.jpg" />
+              <img src="https://img-doubleli.oss-cn-hangzhou.aliyuncs.com/ca1c3331a9d55f408cc4ec184c718bc.jpg" />
+              <img src="https://img-doubleli.oss-cn-hangzhou.aliyuncs.com/8eba916a32dd29152e3667bf5ccf2d6.png" />
+              <img src="https://img-doubleli.oss-cn-hangzhou.aliyuncs.com/bce6341fdd480a3e422fe8155ceb688.png" />
+            </div>
+          </div>
+          <div className={styles.iconLeft} onClick={moveImgbox} name="left">
+            <p name="left">《</p>
+          </div>
+          <div className={styles.iconRight} onClick={moveImgbox} name="right">
+            <p name="right">》</p>
+          </div>
         </Backdrop>
       )}
     </>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,68 +1,87 @@
 /*
  * @Author: dongheheyeye 119031939+dongheheyeye@users.noreply.github.com
  * @Date: 2023-08-04 10:11:05
- * @LastEditors: dongheheyeye 119031939+dongheheyeye@users.noreply.github.com
- * @LastEditTime: 2023-08-09 20:39:47
+ * @LastEditors: DoubleLiHao =2998000782@qq.com
+ * @LastEditTime: 2023-08-10 11:02:39
  * @FilePath: \dom\recruit-web\src\pages\Login\Login.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import styles from './Login.module.css'
-import React from 'react'
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import {Input, Space,Button} from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { useDispatch, useSelector } from 'react-redux';
-import { usePostLoginMutation } from '../../store/loginApi';
-import { enterSuperAdmin,notSuperAdmin,setGroupId } from '../../store/loginInfoSlice';
-import { useNavigate } from 'react-router-dom'
+import styles from "./Login.module.css";
+import React from "react";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Input, Space, Button } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { usePostLoginMutation } from "../../store/loginApi";
+import {
+  enterSuperAdmin,
+  notSuperAdmin,
+  setGroupId,
+} from "../../store/loginInfoSlice";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const [postLoginInfo, message] = usePostLoginMutation()
-    const data = useSelector((state) => state.loginInfoSlice);
-    //post请求返回一个函数，一个存储信息的对象
-    const clickHandler = ()=>{
-        postLoginInfo()
-        console.log(message);
-        //判断账号密码是错误
-        if(false){
-
-            return
-        }
-        //判断是否为超级管理员
-        if(message){
-            dispatch(enterSuperAdmin())
-        }
-        else{
-            dispatch(notSuperAdmin())
-            dispatch(setGroupId())
-        }
-        navigate('/',{replace:true})
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [postLoginInfo, message] = usePostLoginMutation();
+  const data = useSelector((state) => state.loginInfoSlice);
+  //post请求返回一个函数，一个存储信息的对象
+  const clickHandler = () => {
+    postLoginInfo();
+    console.log(message);
+    //判断账号密码是错误
+    if (false) {
+      return;
     }
-    return(
-        <React.Fragment>
-            <div className={styles.container}>
-                    <div className={styles.content}>
-                    <img src="https://donghehe.oss-cn-hangzhou.aliyuncs.com/%E4%BA%91%E9%A1%B6%E4%B9%A6%E9%99%A2.png" alt="" />
+    //判断是否为超级管理员
+    if (message) {
+      dispatch(enterSuperAdmin());
+    } else {
+      dispatch(notSuperAdmin());
+      dispatch(setGroupId());
+    }
+    navigate("/", { replace: true });
+  };
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <img
+            className={styles.img}
+            src="https://donghehe.oss-cn-hangzhou.aliyuncs.com/%E4%BA%91%E9%A1%B6%E4%B9%A6%E9%99%A2.png"
+            alt=""
+          />
 
-                        <div className={styles.content_left}></div>
-                        <div className={styles.content_right}>
-                            <div  className={styles.input_container}> 
-                                <Input placeholder="group" className={styles.group}
-                                prefix={<UserOutlined />} />
-                                 <Input.Password
-                                placeholder="input password"
-                                className={styles.password}
-                                iconRender={(visible) => (visible ? <EyeTwoTone className={styles.password}/> : <EyeInvisibleOutlined className={styles.password}/>)}
-                                />
-                                    <Button type="primary"onClick={clickHandler} className={styles.button} >
-                                    登陆
-                                    </Button>
-                            </div>
-                        </div>
-                    </div>
+          <div className={styles.content_left}></div>
+          <div className={styles.content_right}>
+            <div className={styles.input_container}>
+              <Input
+                placeholder="group"
+                className={styles.group}
+                prefix={<UserOutlined />}
+              />
+              <Input.Password
+                placeholder="input password"
+                className={styles.password}
+                iconRender={(visible) =>
+                  visible ? (
+                    <EyeTwoTone className={styles.password} />
+                  ) : (
+                    <EyeInvisibleOutlined className={styles.password} />
+                  )
+                }
+              />
+              <Button
+                type="primary"
+                onClick={clickHandler}
+                className={styles.button}
+              >
+                登陆
+              </Button>
             </div>
-        </React.Fragment>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
